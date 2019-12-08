d3579a19d3273b4842f354356c9d0802e30b5fce
HosannaGabeOji <gabeoji01@gmail.com>
2019-12-08 19:02:24 +0100

    using @propertySources

--- src/main/java/guru/springframework/demo/DiDemoReviewApplication.java
+++ src/main/java/guru/springframework/demo/DiDemoReviewApplication.java
@@ -2,6 +2,7 @@ package guru.springframework.demo;
 
 import guru.springframework.demo.controllers.MyController;
 import guru.springframework.demo.examplebeans.FakeDataSource;
+import guru.springframework.demo.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -24,6 +25,10 @@ public class DiDemoReviewApplication {
         FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
         System.out.println(fakeDataSource.getUser());
+
+        FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+
+        System.out.println(fakeJmsBroker.getUsername());
 
     }
 
--- src/main/java/guru/springframework/demo/config/PropertyConfig.java
+++ src/main/java/guru/springframework/demo/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package guru.springframework.demo.config;
 
 import guru.springframework.demo.examplebeans.FakeDataSource;
+import guru.springframework.demo.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
@@ -15,7 +17,11 @@ import org.springframework.core.env.Environment;
  * Date: 12/3/2019.
  */
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
     @Autowired
@@ -30,6 +36,15 @@ public class PropertyConfig {
     @Value("${guru.dburl}")
     String url;
 
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Value("${guru.jms.url}")
+    String jmsUrl;
+
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -37,6 +52,15 @@ public class PropertyConfig {
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker jmsBroker = new FakeJmsBroker();
+        jmsBroker.setUsername(jmsUsername);
+        jmsBroker.setPassword(jmsPassword);
+        jmsBroker.setUrl(jmsUrl);
+        return jmsBroker;
     }
 
     @Bean
